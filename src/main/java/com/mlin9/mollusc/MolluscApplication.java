faae514b45d79e7ebfe02d9ea884140789383c90
unknown <297980373+nuwa9@users.noreply.github.com>
2026-09-21 01:39:37 -0400

    initial-commit

---
+++ src/main/java/com/mlin9/mollusc/MolluscApplication.java
@@ -0,0 +1,13 @@
+package com.mlin9.mollusc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MolluscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MolluscApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mlin9/mollusc/MolluscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlin9.mollusc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MolluscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
